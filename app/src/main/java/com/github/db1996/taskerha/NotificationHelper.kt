1ff17b13ae337b75ac5411473bddbd0e780a08b7
dylan PC <dylanbos1996@gmail.com>
2025-11-30 15:14:41 +0100

    Icons cleanup

--- app/src/main/java/com/github/db1996/taskerha/NotificationHelper.kt
+++ app/src/main/java/com/github/db1996/taskerha/NotificationHelper.kt
@@ -43,7 +43,7 @@ object NotificationHelper {
         }
 
         val builder = NotificationCompat.Builder(context, ERROR_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_notification_icon)
+            .setSmallIcon(R.drawable.ic_notification_icon2)
             .setContentTitle(title)
             .setContentText(content)
             .setStyle(NotificationCompat.BigTextStyle().bigText(content))
