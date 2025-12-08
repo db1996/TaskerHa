3949a98ea91db3af7b5d1466cd826fdca98d3e52
db1996 <dylanbos1996@gmail.com>
2025-12-08 14:46:11 +0100

    Added fastlane files (f-droid)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization) // 👈 this line
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.db1996.taskerha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 10000
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
