1265306660c7ed9ff250b921336ff175403e9964
dylan PC <dylanbos1996@gmail.com>
2026-05-01 20:29:47 +0200

    updated version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.db1996.taskerha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10130
-        versionName = "1.1.3"
+        versionCode = 10140
+        versionName = "1.1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
