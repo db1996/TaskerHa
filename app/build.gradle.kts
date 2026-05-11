def40d810bfe1512e94a08776b380868a519e877
dylan PC <dylanbos1996@gmail.com>
2026-05-11 23:55:03 +0200

    Version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.github.db1996.taskerha"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10160
-        versionName = "1.1.6"
+        versionCode = 10200
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
