98159b29d48b6384ae4597b49814a7d9ef3675d1
dylan PC <dylanbos1996@gmail.com>
2026-06-15 20:14:34 +0200

    Changed order of links in about section

--- app/src/main/java/com/github/db1996/taskerha/activities/screens/MainSettingsScreen.kt
+++ app/src/main/java/com/github/db1996/taskerha/activities/screens/MainSettingsScreen.kt
@@ -1507,13 +1507,13 @@ private fun OptionsTab() {
         Card(modifier = Modifier.fillMaxWidth()) {
             Column(modifier = Modifier.fillMaxWidth()) {
                 AboutLinkRow(
-                    label = "Repository",
-                    url = "https://github.com/db1996/TaskerHa"
+                    label = "Documentation",
+                    url = "https://taskerha.db1996-gh.com/"
                 )
                 HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
                 AboutLinkRow(
-                    label = "Documentation",
-                    url = "https://taskerha.db1996-gh.com/"
+                    label = "Repository",
+                    url = "https://github.com/db1996/TaskerHa"
                 )
                 HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
                 AboutLinkRow(
